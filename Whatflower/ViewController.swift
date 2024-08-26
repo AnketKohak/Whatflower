7cd135410236394d2c5d7eb38b07e078dd12a12c
AnketKohak <anketkohak40@gmail.com>
2024-08-26 16:49:34 +0530

    Setting up a UIImagePickerController 333

--- Whatflower/ViewController.swift
+++ Whatflower/ViewController.swift
@@ -9,13 +9,25 @@ import UIKit
 
 class ViewController: UIViewController , UINavigationControllerDelegate,UIImagePickerControllerDelegate{
     let imagepicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         imagepicker.delegate = self
+        imagepicker.allowsEditing = false
+        imagepicker.sourceType = .photoLibrary
     }
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let userpickedImage = info[UIImagePickerController.InfoKey.originalImage]
+        imageView?.image = userpickedImage as? UIImage
+        imagepicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
     @IBAction func cammeraTapped(_ sender: UIBarButtonItem) {
+        present(imagepicker,animated: true,completion: nil)
+        
     }
     
 }
