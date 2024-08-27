ef2851ad21d855aa726ed2a1dd42fbc818090e10
AnketKohak <anketkohak40@gmail.com>
2024-08-27 09:57:00 +0530

    this project is terminated due to unable to caffe model convert to coreml model

--- Whatflower/ViewController.swift
+++ Whatflower/ViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-
+import CoreML
+import Vision
 class ViewController: UIViewController , UINavigationControllerDelegate,UIImagePickerControllerDelegate{
     let imagepicker = UIImagePickerController()
     
@@ -19,11 +20,41 @@ class ViewController: UIViewController , UINavigationControllerDelegate,UIImageP
         imagepicker.sourceType = .photoLibrary
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let userpickedImage = info[UIImagePickerController.InfoKey.originalImage]
-        imageView?.image = userpickedImage as? UIImage
+        if  let userpickedImage = info[UIImagePickerController.InfoKey.originalImage]  as? UIImage{
+            guard let convertedToCIImage =  CIImage(image: userpickedImage) else{
+                fatalError("cannot convert to image")
+            }
+            
+            detect(image: convertedToCIImage)
+            
+            imageView?.image = userpickedImage
+            
+
+        }
         imagepicker.dismiss(animated: true, completion: nil)
     }
     
+    
+    
+    func detect(image : CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else{
+            fatalError("cannot import model")
+            
+        }
+        let request = VNCoreMLRequest(model: model) { request, error in
+            let classification = request.results?.first as? VNClassificationObservation
+            self.navigationItem.title =  classification?.identifier
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+        
+    }
     
     @IBAction func cammeraTapped(_ sender: UIBarButtonItem) {
         present(imagepicker,animated: true,completion: nil)
